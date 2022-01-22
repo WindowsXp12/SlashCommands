2038f8edc0da455cd9c74622d4cca717f6e66cc6
WindowsXp12 <95308332+WindowsXp12@users.noreply.github.com>
2022-01-22 17:42:06 +0100

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,33 @@
+const client = require("../index");
+
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.isCommand()) {
+        await interaction.deferReply({ ephemeral: false }).catch(() => {});
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+        if (!cmd)
+            return interaction.followUp({ content: "An error has occured " });
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
+
+        if(!interaction.member.permissions.has(cmd.userPermission || [])) return interaction.followUp({ content: `You Dont Have Permission To Use This Command` })
+
+        cmd.run(client, interaction, args);
+    }
+
+    if (interaction.isContextMenu()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
+    }
+});
