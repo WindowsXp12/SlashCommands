21700be24e5f9c38beb84f04e6eaaf542038cddc
WindowsXp12 <95308332+WindowsXp12@users.noreply.github.com>
2022-01-22 17:44:00 +0100

    Add files via upload

---
+++ events/channelDelete.js
@@ -0,0 +1,21 @@
+const { MessageEmbed, Discord, Collection } = require('discord.js');
+const db = require('pro.db');
+const client = require('../index.js');
+
+
+client.on('channelDelete', async (channel) => {
+    let color = db.get(`color_${channel.guild.id}`) || "RED";
+        let room = await db.get(`log_${channel.guild.id}`)
+        if (room) {
+    let embed = new MessageEmbed()
+    .setAuthor(channel.guild.name, channel.guild.iconURL({dynamic:true}))
+    .addField(`New Channel Has Been Deleted `,`${channel.name}`,true)
+    .setTimestamp()
+    .setColor(color)
+    .setFooter(channel.guild.name, channel.guild.iconURL({dynamic:true}))
+    channel.guild.channels.cache.get(room).send({ embeds: [embed] });
+        } else {
+            return;
+        }
+    
+})
